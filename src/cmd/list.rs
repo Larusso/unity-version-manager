e6e72ab95aa6213ab21774e3664373936ee58570
Manfred Endres <manfred.endres@tslarusso.de>
2018-03-09 23:07:43 +0100

    Add slightly verbose regex

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::fs;
+use regex::Regex;
 // struct Version {
 //     major: u32,
 //     minor: u32,
@@ -22,11 +23,19 @@ const UNITY_INSTALL_LOCATION : &'static str = "/Applications";
 
 pub fn list() {
     let install_location = Path::new(UNITY_INSTALL_LOCATION);
+
+    let version_pattern = Regex::new(r"(\d+\.\d+\.\d+((f|p|b)\d+)?)$").unwrap();
+
     if let Ok(entries) = fs::read_dir(install_location) {
         for entry in entries {
             if let Ok(entry) = entry {
-                if entry.file_name().to_str().unwrap().starts_with("Unity-") {
-                    println!("{:?}", entry.file_name());
+                if let Some(file_name) = entry.file_name().to_str() {
+                    if file_name.starts_with("Unity-") {
+                        if let Some(caps) = version_pattern.captures(file_name) {
+                            let version = caps.get(1).map_or("", |m| m.as_str());
+                            println!("{:?}", version);
+                        }
+                    }
                 }
             }
         }
