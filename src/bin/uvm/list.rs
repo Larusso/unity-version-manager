0b2669aa84fe56c1cf4970fa679c6b01b3cc89cd
Manfred Endres <manfred.endres@tslarusso.de>
2018-03-13 21:19:21 +0100

    Add console colors

--- src/bin/uvm/list.rs
+++ src/bin/uvm/list.rs
@@ -1,7 +1,9 @@
-#[macro_use]
 extern crate uvm;
+extern crate console;
+
 use uvm::cmd::list::list;
 use uvm::cmd::list::Installation;
+use console::style;
 
 const USAGE: &'static str = "
 uvm-list - List installed unity versions.
@@ -16,24 +18,37 @@ Options:
 ";
 
 fn longest_version(installations: &Vec<Installation>) -> usize {
-    match installations.iter().map(|i| i.version.to_string().len()).max() {
+    match installations
+        .iter()
+        .map(|i| i.version.to_string().len())
+        .max()
+    {
         Some(l) => l,
         None => 0,
     }
 }
 
 fn main() {
-  let o = uvm::cli::get_list_options(USAGE);
-  if let Ok(installations) = list() {
-    let verbose = o.unwrap_or(uvm::cli::ListOptions{verbose:false}).verbose;
-    let longest_version = longest_version(&installations);
-    for installation in installations {
-        if verbose {
-            println!("{version:>width$} - {path}", version = installation.version.to_string(), width = longest_version, path = installation.path.display());
-        }
-        else {
-            println!("{version:>width$}", version = installation.version.to_string(), width = longest_version);
+    let o = uvm::cli::get_list_options(USAGE);
+    if let Ok(installations) = list() {
+        let verbose = o.unwrap_or(uvm::cli::ListOptions { verbose: false })
+            .verbose;
+        let longest_version = longest_version(&installations);
+        for installation in installations {
+            if verbose {
+                println!(
+                    "{version:>width$} - {path}",
+                    version = style(installation.version.to_string()).cyan(),
+                    width = longest_version,
+                    path = style(installation.path.display()).italic().green()
+                );
+            } else {
+                println!(
+                    "{version:>width$}",
+                    version = style(installation.version.to_string()).cyan(),
+                    width = longest_version
+                );
+            }
         }
     }
-  }
 }
