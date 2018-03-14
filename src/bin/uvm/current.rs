d79d027ff1a3320f29df28e12bcdbb12bc4ee24d
Manfred Endres <manfred.endres@tslarusso.de>
2018-03-14 12:49:20 +0100

    Draft implementation of uvm current

---
+++ src/bin/uvm/current.rs
@@ -0,0 +1,57 @@
+extern crate uvm;
+extern crate console;
+
+use std::path::Path;
+use uvm::unity::{Installation, Version};
+use console::style;
+use std::str::FromStr;
+use console::Term;
+
+const USAGE: &'static str = "
+uvm-current - Prints current activated version of unity.
+
+Usage:
+  uvm-current [options]
+  uvm-current (-h | --help)
+
+Options:
+  -v, --verbose     print more output
+  -h, --help        show this help message and exit
+";
+
+fn main() {
+    let o = uvm::cli::get_list_options(USAGE);
+    let path = Path::new("/Applications/Unity");
+    let errorTerm = Term::stderr();
+    let outTerm = Term::stdout();
+    if let Ok(metadata) = path.symlink_metadata() {
+        if metadata.file_type().is_symlink() {
+            let linked_file = path.read_link().unwrap();
+            let name = linked_file.file_name().unwrap().to_str().expect("Error while reading filename");
+            let version = Version::from_str(name).expect("link is not a unity version");
+            let installation = Installation{version: version, path: path.read_link().unwrap()};
+            let verbose = o.unwrap_or(uvm::cli::ListOptions { verbose: false }).verbose;
+            let line = if verbose {
+                format!(
+                    "{version} - {path}",
+                    version = style(installation.version.to_string()).cyan(),
+                    path = style(installation.path.display()).italic().green()
+                )
+            } else {
+                format!(
+                    "{version}",
+                    version = style(installation.version.to_string()).cyan(),
+                )
+            };
+            outTerm.write_line(&line);
+
+        }
+        else {
+            errorTerm.write_line("/Applications/Unity is not a symlink");
+        }
+    }
+    else {
+        errorTerm.write_line("No active version");
+    }
+
+}
