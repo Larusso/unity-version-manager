5d534d9ab620ae476dd7a72949ba992127eb46a2
Manfred Endres <manfred.endres@tslarusso.de>
2018-03-14 11:44:01 +0100

    Use term instead of println! for output

--- src/bin/uvm/list.rs
+++ src/bin/uvm/list.rs
@@ -4,6 +4,8 @@ extern crate console;
 use uvm::cmd::list::list;
 use uvm::unity::Installation;
 use console::style;
+use console::Term;
+use console::pad_str;
 
 const USAGE: &'static str = "
 uvm-list - List installed unity versions.
@@ -30,25 +32,26 @@ fn longest_version(installations: &Vec<Installation>) -> usize {
 
 fn main() {
     let o = uvm::cli::get_list_options(USAGE);
+    let term = Term::stdout();
     if let Ok(installations) = list() {
-        let verbose = o.unwrap_or(uvm::cli::ListOptions { verbose: false })
-            .verbose;
+        let verbose = o.unwrap_or(uvm::cli::ListOptions { verbose: false }).verbose;
         let longest_version = longest_version(&installations);
         for installation in installations {
-            if verbose {
-                println!(
+            let line = if verbose {
+                format!(
                     "{version:>width$} - {path}",
                     version = style(installation.version.to_string()).cyan(),
                     width = longest_version,
                     path = style(installation.path.display()).italic().green()
-                );
+                )
             } else {
-                println!(
+                format!(
                     "{version:>width$}",
                     version = style(installation.version.to_string()).cyan(),
                     width = longest_version
-                );
-            }
+                )
+            };
+            term.write_line(&line);
         }
     }
 }
