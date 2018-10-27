7dcfcce06a51b65ad8a891d846bed93ffd8880f8
Manfred Endres <manfred.endres@tslarusso.de>
2018-10-28 00:05:39 +0200

    First attempts to call native windows API

--- uvm_core/src/unity/version/win.rs
+++ uvm_core/src/unity/version/win.rs
@@ -2,12 +2,16 @@ use std::path::PathBuf;
 use super::*;
 
 extern crate winapi;
-use self::winapi::um::winver::GetFileVersionInfoA;
-use self::winapi::um::winver::GetFileVersionInfoSizeA;
-use self::winapi::um::winver::VerQueryValueA;
+use self::winapi::um::winver;
+use self::winapi::shared::minwindef::DWORD;
+use self::winapi::um::winnt::LPCSTR;
 
 pub fn get_unity_version(path:PathBuf) -> Option<Version> {
+    let bytes = path.to_string_lossy().into_bytes() + b"\0";
+    let cchars = bytes.map_in_place(|b| b as c_char);
     unsafe {
+        let mut dummy:DWORD = std::ptr::null_mut();
+        let version_size:DWORD = winver::GetFileVersionInfoSize(cchars, &dummy);
         None
     }
 }
