eb2f8e8655fd131d85e58b107485035d370c240f
Manfred Endres <manfred.endres@tslarusso.de>
2018-10-27 23:08:13 +0200

    List unity hub installations

--- uvm_core/src/unity/hub/mod.rs
+++ uvm_core/src/unity/hub/mod.rs
@@ -2,9 +2,16 @@ mod settings;
 pub mod editors;
 mod paths;
 
-use unity::Installation;
-use unity::Result;
+use std::io;
+use unity;
 //
-// pub fn list_installations() -> Result<Installations> {
-//     list_installations_in_dir(paths::)
-// }
+
+pub fn list_installations() -> unity::Result<unity::Installations> {
+    let install_path = paths::install_path().ok_or_else(|| {
+        io::Error::new(io::ErrorKind::NotFound, "install path not found")
+    })?;
+    let editors = editors::Editors::load()?;
+    let installations = unity::list_installations_in_dir(&install_path)?;
+    let iter = installations.chain(unity::Installations::from(editors));
+    Ok(unity::Installations(Box::new(iter)))
+}
