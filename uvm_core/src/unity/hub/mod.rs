ef53a198cb0aa3c97b036dbdb73426abf90901f6
Manfred Endres <manfred.endres@tslarusso.de>
2018-10-29 09:11:58 +0100

    Hub installations return empty iterator
    when unity hub is not installed

--- uvm_core/src/unity/hub/mod.rs
+++ uvm_core/src/unity/hub/mod.rs
@@ -9,8 +9,15 @@ pub fn list_installations() -> unity::Result<unity::Installations> {
     let install_path = paths::install_path().ok_or_else(|| {
         io::Error::new(io::ErrorKind::NotFound, "install path not found")
     })?;
+    debug!("unity hub install path: {}", install_path.display());
+
     let editors = editors::Editors::load()?;
-    let installations = unity::list_installations_in_dir(&install_path)?;
-    let iter = installations.chain(unity::Installations::from(editors));
-    Ok(unity::Installations(Box::new(iter)))
+    debug!("raw editors map: {:?}", editors);
+    let editors = unity::Installations::from(editors);
+    if let Ok(installations) = unity::list_installations_in_dir(&install_path) {
+        let iter = installations.chain(editors);
+        return Ok(unity::Installations(Box::new(iter)))
+    }
+
+    Ok(editors)
 }
