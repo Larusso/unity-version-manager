51fc4b005b4f0edaec810480dc7ba076fb0aa0c5
Manfred Endres <manfred.endres@tslarusso.de>
2018-11-21 16:28:59 +0100

    Improve versions command default output by applying filter
    Description
    ===========
    
    The `versions` command output showed by default all available `final`
    versions. This list will be auto updated every week in the not so
    distant future. The most valuable output would be the latest versions
    from each major release and release type (final, patch, beta, alpha).
    This patch makes this changes by filtering the list. The cli switch
    `--all` in combination with one of the release type flags works now
    different.
    
    ```
    $uvm versions
    Latest versions to install:
    5.6.4f1
    5.6.4p1
    2019.1.0a7
    2018.3.0b8
    ...
    ```
    
    ```
    $uvm versions --all
    All available versions to install:
    5.4.5f1
    5.5.0f3
    5.5.1f1
    5.5.2p1
    5.5.2f1
    5.5.3p2
    5.5.3f1
    5.5.4f1
    5.5.5f1
    5.6.0p2
    ...
    ```
    
    ```
    $uvm versions --final
    Latest final versions to install:
    2017.4.14f1
    2018.2.14f1
    5.6.4f1
    ```
    
    ```
    $uvm versions --final --beta
    Latest beta, final versions to install:
    2017.3.0b11
    2017.4.14f1
    5.6.4f1
    2018.3.0b8
    2018.2.14f1
    ```
    
    ```
    $uvm versions --final --all
    All available final versions to install:
    5.4.5f1
    5.5.0f3
    5.5.1f1
    5.5.2f1
    5.5.3f1
    5.5.4f1
    5.5.5f1
    5.6.0f3
    5.6.1f1
    5.6.2f1
    5.6.3f1
    5.6.4f1
    2017.1.0f1
    2017.1.0f2
    2017.1.0f3
    2017.1.1f1
    2017.1.2f1
    2017.1.3f1
    ...
    ```
    
    This gives the most flexibility when calling the command.
    
    Changes
    =======
    
    ![IMPROVE] versions command by filtering newest versions by default
    ![CHANGE] cli default settings and meaning of the `--all` flag

--- install/uvm-versions/src/lib.rs
+++ install/uvm-versions/src/lib.rs
@@ -10,9 +10,10 @@ use console::Style;
 use console::Term;
 use indicatif::{ProgressBar, ProgressStyle};
 use itertools::Itertools;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::io;
 use uvm_cli::ColorOption;
+use uvm_core::unity::Version;
 use uvm_core::unity::VersionType;
 
 #[derive(Debug, Deserialize)]
@@ -28,24 +29,40 @@ pub struct VersionsOptions {
 
 impl VersionsOptions {
     pub fn list_variants(&self) -> HashSet<VersionType> {
-        let mut variants: HashSet<VersionType> = HashSet::with_capacity(3);
+        let mut variants: HashSet<VersionType> = HashSet::with_capacity(4);
 
-        if self.flag_alpha || self.flag_all {
+        if self.has_variant_flags() {
+            if self.flag_alpha {
+                variants.insert(VersionType::Alpha);
+            }
+
+            if self.flag_beta {
+                variants.insert(VersionType::Beta);
+            }
+
+            if self.flag_patch {
+                variants.insert(VersionType::Patch);
+            }
+
+            if self.flag_final {
+                variants.insert(VersionType::Final);
+            }
+        } else {
             variants.insert(VersionType::Alpha);
-        }
-
-        if self.flag_beta || self.flag_all {
             variants.insert(VersionType::Beta);
-        }
-
-        if self.flag_patch || self.flag_all {
             variants.insert(VersionType::Patch);
-        }
-
-        if self.flag_final || self.flag_all || variants.is_empty() {
             variants.insert(VersionType::Final);
         }
+
         return variants;
+    }
+
+    fn has_variant_flags(&self) -> bool {
+        self.flag_alpha || self.flag_beta || self.flag_patch || self.flag_final
+    }
+
+    pub fn filter_versions(&self) -> bool {
+        !self.flag_all
     }
 }
 
@@ -72,8 +89,23 @@ impl UvmCommand {
         }
     }
 
+    fn out_message(&self, options: &VersionsOptions) -> String {
+        let variants = options.list_variants();
+        let variants_format = match options.list_variants().len() {
+            4 => "".to_string(),
+            _ => format!("{:#} ", &variants.iter().format(", ")),
+        };
+
+        if options.filter_versions() {
+            format!("Latest {}versions to install:", variants_format)
+        } else {
+            format!("All available {}versions to install:", variants_format)
+        }
+    }
+
     pub fn exec(&self, options: VersionsOptions) -> io::Result<()> {
         let out_style = Style::new().cyan();
+        let message_style = Style::new().green().bold();
 
         let variants = options.list_variants();
         let bar = ProgressBar::new_spinner();
@@ -89,10 +121,62 @@ impl UvmCommand {
         bar.tick();
 
         let versions = uvm_core::unity::all_versions()?;
+        let versions: Vec<Version> = if options.filter_versions() {
+            let version_type: HashMap<u64, HashSet<Version>> = HashMap::with_capacity(10);
+            let version_type = versions.fold(version_type, |mut version_type_map, version| {
+                let major = version.major();
+                let mut versions: HashSet<Version> = HashSet::with_capacity(1);
+                versions.insert(version);
+
+                if let Some(old_versions) = version_type_map.insert(major, versions) {
+                    let mut versions = version_type_map.get_mut(&major).unwrap();
+                    for v in old_versions {
+                        versions.insert(v);
+                    }
+                }
+
+                version_type_map
+            });
+
+            let mut versions_filter: HashMap<u64, HashMap<VersionType, Version>> =
+                HashMap::with_capacity(10);
+            let versions_filter =
+                version_type
+                    .into_iter()
+                    .fold(versions_filter, |mut filter, (major, versions)| {
+                        let type_map: HashMap<VersionType, Version> = HashMap::new();
+                        filter.insert(
+                            major,
+                            versions.into_iter().fold(type_map, |mut map, version| {
+                                let t = *version.release_type();
+                                let needs_update = match map.get(&t) {
+                                    Some(v) if v > &version => false,
+                                    _ => true,
+                                };
+
+                                if needs_update {
+                                    map.insert(t, version);
+                                }
+                                map
+                            }),
+                        );
+                        filter
+                    });
+
+            versions_filter
+                .into_iter()
+                .flat_map(|(_major, types)| types.into_iter().map(|(_t, version)| version))
+                .collect()
+        } else {
+            versions.collect()
+        };
+
         bar.finish_and_clear();
 
-        self.stderr
-            .write_line("Available Unity versions to install:")?;
+        self.stderr.write_line(&format!(
+            "{}",
+            message_style.apply_to(self.out_message(&options))
+        ))?;
         for version in versions {
             if variants.contains(version.release_type()) {
                 self.stdout
