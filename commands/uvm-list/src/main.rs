c3304919decdad353ae1b777559d1b1336a33558
Manfred Endres <manfred.endres@tslarusso.de>
2021-03-01 21:33:44 +0100

    Use structops instead of docopts for uvm-list and uvm-help
    Description
    ===========
    
    Use structops for `uvm-help` and `uvm-list`. This makes the setup
    simpler.
    
    Changes
    =======
    
    * ![IMPROVE] use `structops` over `docopts` in `uvm-list` command
    * ![IMPROVE] use `structops` over `docopts` in `uvm-help` command

--- commands/uvm-list/src/main.rs
+++ commands/uvm-list/src/main.rs
@@ -1,26 +1,97 @@
+use anyhow::{Context, Result};
+use console::Style;
+use log::info;
+use std::io;
+use structopt::{clap::crate_authors, clap::crate_description, clap::crate_version, StructOpt};
 use uvm_cli;
-use uvm_list;
+use uvm_cli::{options::ColorOption, set_colors_enabled, set_loglevel};
 
-use uvm_list::ListOptions;
+#[derive(StructOpt, Debug)]
+#[structopt(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
+struct Opts {
+    /// print only the path to the current version
+    #[structopt(short, long = "path")]
+    path_only: bool,
 
-const USAGE: &str = "
-uvm-list - List installed unity versions.
+    /// print more output
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: i32,
 
-Usage:
-  uvm-list [options]
-  uvm-list (-h | --help)
+    /// print debug output
+    #[structopt(short, long)]
+    debug: bool,
 
-Options:
-  -p, --path        print only the path to the current version
-  -v, --verbose     print more output
-  -d, --debug       print debug output
-  --hub             print unity hub installations
-  --all             print all unity installations
-  --color WHEN      Coloring: auto, always, never [default: auto]
-  -h, --help        show this help message and exit
-";
+    /// print unity hub installations
+    #[structopt(long = "hub")]
+    use_hub: bool,
 
-fn main() {
-    let options: ListOptions = uvm_cli::get_options(USAGE).unwrap();
-    uvm_list::UvmCommand::new().exec(&options).unwrap();
+    /// print all unity installations
+    #[structopt(long)]
+    all: bool,
+
+    /// Color:.
+    #[structopt(short, long, possible_values = &ColorOption::variants(), case_insensitive = true, default_value)]
+    color: ColorOption,
+}
+
+fn main() -> Result<()> {
+    let opt = Opts::from_args_safe().map(|opt| {
+        set_colors_enabled(&opt.color);
+        set_loglevel(opt.debug.then(|| 2).unwrap_or(opt.verbose));
+        opt
+    })?;
+
+    list(&opt).context("failed to list Unity installations")?;
+    Ok(())
+}
+
+fn list(options: &Opts) -> io::Result<()> {
+    let current_version = uvm_core::current_installation().ok();
+    let list_function = if options.all {
+        info!("fetch all installations");
+        uvm_core::list_all_installations
+    } else if options.use_hub {
+        info!("fetch installations from unity hub");
+        uvm_core::list_hub_installations
+    } else {
+        info!("fetch installations from uvm");
+        uvm_core::list_installations
+    };
+
+    if let Ok(installations) = list_function() {
+        eprintln!("Installed Unity versions:");
+        let verbose = options.verbose;
+        let path_only = options.path_only;
+
+        let output = installations.fold(String::new(), |out, installation| {
+            let mut out_style = Style::new().cyan();
+            let mut path_style = Style::new().italic().green();
+
+            if let Some(ref current) = &current_version {
+                if current == &installation {
+                    out_style = out_style.yellow().bold();
+                    path_style = path_style.italic().yellow();
+                }
+            }
+            let mut new_line = out;
+
+            if !path_only {
+                new_line += &format!("{}", out_style.apply_to(installation.version().to_string()));
+            }
+
+            if verbose > 0 {
+                new_line += " - ";
+            }
+
+            if verbose > 0 || path_only {
+                new_line += &format!("{}", path_style.apply_to(installation.path().display()));
+            }
+            new_line += "\n";
+            new_line
+        });
+
+        eprintln!("{}", &output);
+    };
+
+    Ok(())
 }
