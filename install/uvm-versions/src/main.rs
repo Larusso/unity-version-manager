04f2073a58ae97a79ac0cf099fdb5e93901bc6c5
Manfred Endres <2523575+Larusso@users.noreply.github.com>
2021-11-01 08:28:49 +0100

    Improve uvm-versions command (#293)
    Description
    ===========
    
    Switch to StructOpt based commandline parser.
    Cleanup code.

--- install/uvm-versions/src/main.rs
+++ install/uvm-versions/src/main.rs
@@ -1,44 +1,104 @@
-use uvm_cli;
+use anyhow::{Context, Result};
+use regex::Regex;
+use std::collections::HashSet;
+use structopt::{clap::crate_authors, clap::crate_description, clap::crate_version, StructOpt};
+use uvm_cli::{options::ColorOption, set_colors_enabled, set_loglevel};
+use uvm_core::unity::VersionType;
 use uvm_versions;
-#[macro_use]
-extern crate log;
 
+#[derive(StructOpt, Debug)]
+#[structopt(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
+struct Opts {
+    /// list all available versions for the selected version types
+    #[structopt(short, long)]
+    all: bool,
 
-use console::style;
-use std::process;
-use uvm_versions::VersionsOptions;
+    /// list final versions 
+    #[structopt(short = "f", long = "final")]
+    list_final: bool,
 
-const USAGE: &str = "
-uvm-versions - List available Unity versions to install.
+    /// list beta versions 
+    #[structopt(short = "b", long = "beta")]
+    list_beta: bool,
 
-Usage:
-  uvm-versions [options] [<pattern>]
-  uvm-versions (-h | --help)
+    /// list alpha versions 
+    #[structopt(long = "alpha")]
+    list_alpha: bool,
 
-Arguments:
-  pattern           a regex pattern to filter the result
+    /// list patch versions 
+    #[structopt(short = "p", long = "patch")]
+    list_patch: bool,
 
-Options:
-  -a, --all         list all available versions for the selected version types
-  -f, --final       list final versions
-  -b, --beta        list beta versions
-  --alpha           list alpha versions
-  -p, --patch       list patch versions
-  -v, --verbose     print more output
-  -d, --debug       print debug output
-  --color WHEN      Coloring: auto, always, never [default: auto]
-  -h, --help        show this help message and exit
-";
+    /// a regex pattern to filter results
+    #[structopt()]
+    pattern: Option<Regex>,
 
-fn main() -> std::io::Result<()> {
-    let options: VersionsOptions = uvm_cli::get_options(USAGE)?;
-    uvm_versions::UvmCommand::new()
-        .exec(&options)
-        .unwrap_or_else(|err| {
-            let message = "Failure listing available versions";
-            eprintln!("{}", style(message).red());
-            info!("{}", &format!("{}", style(err).red()));
-            process::exit(1);
-        });
+    /// print debug output
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// print more output
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: i32,
+
+    /// Color:.
+    #[structopt(short, long, possible_values = &ColorOption::variants(), case_insensitive = true, default_value)]
+    color: ColorOption,
+}
+
+impl uvm_versions::VersionListOptions for Opts {
+  fn list_variants(&self) -> HashSet<VersionType> {
+    let mut variants: HashSet<VersionType> = HashSet::with_capacity(4);
+
+    if self.has_variant_flags() {
+        if self.list_alpha {
+            variants.insert(VersionType::Alpha);
+        }
+
+        if self.list_beta{
+            variants.insert(VersionType::Beta);
+        }
+
+        if self.list_patch {
+            variants.insert(VersionType::Patch);
+        }
+
+        if self.list_final {
+            variants.insert(VersionType::Final);
+        }
+    } else {
+        variants.insert(VersionType::Alpha);
+        variants.insert(VersionType::Beta);
+        variants.insert(VersionType::Patch);
+        variants.insert(VersionType::Final);
+    }
+    variants
+  }
+
+  fn has_variant_flags(&self) -> bool {
+      self.list_alpha || self.list_beta || self.list_patch || self.list_final
+  }
+
+  fn filter_versions(&self) -> bool {
+      !self.all
+  }
+
+  fn pattern(&self) -> &Option<Regex> {
+      &self.pattern
+  }
+
+  fn debug(&self) -> bool {
+    self.debug
+  }
+}
+
+fn main() -> Result<()> {
+    let opt = Opts::from_args_safe().map(|opt| {
+        set_colors_enabled(&opt.color);
+        set_loglevel(opt.debug.then(|| 2).unwrap_or(opt.verbose));
+        opt
+    })?;
+
+    uvm_versions::list_versions(&opt).context("failed to list Unity modules")?;
     Ok(())
 }
