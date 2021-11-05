d2eea5e4e50bf62db8f9e92a76433f7c6524602d
Manfred Endres <manfred.endres@tslarusso.de>
2021-11-05 20:21:16 +0100

    Improve uvm-fix-modules-json helper command
    Description
    ===========
    
    Switch from docopt to structopt for generating the cli.
    Remove now unneeded lib file in command crate.

--- commands/uvm-fix-modules-json/src/main.rs
+++ commands/uvm-fix-modules-json/src/main.rs
@@ -1,38 +1,48 @@
+use anyhow::{Context, Result};
 use console::style;
 use log::{info, trace};
 use std::fs::OpenOptions;
-use std::io::Result;
 use std::process;
 use uvm_cli;
 use uvm_core;
-use uvm_core::unity::{Installations, Manifest};
-use uvm_fix_modules_json::Options;
+use uvm_core::unity::{Installations, Manifest, Version};
+use uvm_cli::{options::ColorOption, set_colors_enabled, set_loglevel};
+use structopt::{clap::crate_authors, clap::crate_description, clap::crate_version, StructOpt};
 
-const USAGE: &str = "
-uvm-fix-modules-json - Write the modules.json for the given unity version.
+#[derive(StructOpt, Debug)]
+#[structopt(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
+struct Opts {
+    #[structopt(name = "version", group = "input", required_unless("all"))]
+    version: Vec<Version>,
 
-Usage:
-  uvm-fix-modules-json [options] (--all | <version>...)
-  uvm-fix-modules-json (-h | --help)
+    /// only print moddule to stdout
+    #[structopt(long="dry-run")]
+    dry_run: bool,
 
-Options:
-  --dry-run                         only prints modules to stdout
-  --all                             generate modules.json for all installed editors
-  -v, --verbose                     print more output
-  -d, --debug                       print debug output
-  --color WHEN                      Coloring: auto, always, never [default: auto]
-  -h, --help                        show this help message and exit
-";
+    /// generate modules.json for all installed editors
+    #[structopt(short, long, group = "input")]
+    all: bool,
 
-fn generate_for_installation(options: Options) -> Result<()> {
-    let installations = if options.all() {
+    /// print debug output
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// print more output
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: i32,
+
+    /// Color:.
+    #[structopt(short, long, possible_values = &ColorOption::variants(), case_insensitive = true, default_value)]
+    color: ColorOption,
+}
+
+fn generate_for_installation(options: Opts) -> Result<()> {
+    let installations = if options.all {
         info!("generate modules.json for all installations");
         uvm_core::list_all_installations()
     } else {
         let v = options
-            .version()
-            .as_ref()
-            .expect("expect a provided version");
+            .version;
         let installations: Installations = v
             .iter()
             .flat_map(|v| uvm_core::find_installation(v).into_iter())
@@ -47,7 +57,7 @@ fn generate_for_installation(options: Options) -> Result<()> {
         let c = i.installed_components();
         manifest.mark_installed_modules(c);
         let output_path = i.path().join("modules.json");
-        if options.dry_run() {
+        if options.dry_run {
             eprintln!("modules json for {}", &i.version());
             eprintln!("output path: {}", output_path.display());
             println!("{}", manifest.modules_json()?);
@@ -64,11 +74,16 @@ fn generate_for_installation(options: Options) -> Result<()> {
     Ok(())
 }
 
-fn main() {
-    let options: Options = uvm_cli::get_options(USAGE).unwrap();
+fn main() -> Result<()> {
+    let opt = Opts::from_args_safe().map(|opt| {
+        set_colors_enabled(&opt.color);
+        set_loglevel(opt.debug.then(|| 2).unwrap_or(opt.verbose));
+        opt
+    })?;
+
     trace!("generate modules.json");
 
-    generate_for_installation(options).unwrap_or_else(|err| {
+    generate_for_installation(opt).unwrap_or_else(|err| {
         let message = "Unable generate modules.json";
         eprintln!("{}", style(message).red());
         eprintln!("{}", style(err).red());
@@ -76,4 +91,5 @@ fn main() {
     });
 
     eprintln!("{}", style("Done").green());
+    Ok(())
 }
