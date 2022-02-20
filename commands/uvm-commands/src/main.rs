ff7810b89e41a8a69882a3fcc33f382e1c373a95
Manfred Endres <manfred.endres@tslarusso.de>
2022-02-20 21:13:27 +0100

    Improve uvm-commands command
    Description
    ===========
    
    Switch from docopt to structopt for cli interface.
    Filter commands list to only show unique command names.
    Use commands located next to the binaries over commands fetched
    from path.

--- commands/uvm-commands/src/main.rs
+++ commands/uvm-commands/src/main.rs
@@ -1,25 +1,83 @@
+use anyhow::Result;
+use console::Style;
+use console::Term;
 use uvm_cli;
-use uvm_commands;
 
-use uvm_commands::CommandsOptions;
+use structopt::{
+  clap::crate_authors, clap::crate_description, clap::crate_version, clap::AppSettings, StructOpt,
+};
+use uvm_cli::{options::ColorOption, set_colors_enabled, set_loglevel};
 
-const USAGE: &str = "
-uvm-commands - Lists all available sub commands.
+const SETTINGS: &'static [AppSettings] = &[
+  AppSettings::ColoredHelp,
+  AppSettings::DontCollapseArgsInUsage,
+];
 
-Usage:
-  uvm-commands [options]
-  uvm-commands (-h | --help)
+#[derive(StructOpt, Debug)]
+#[structopt(version = crate_version!(), author = crate_authors!(), about = crate_description!(), settings = SETTINGS)]
+struct Opts {
+  /// print a list with commands
+  #[structopt(short, long)]
+  list: bool,
 
-Options:
-  -l, --list        print a list with commands
-  -1                single column list
-  -p, --path        print only the path to the commands
-  -v, --verbose     print more output
-  --color WHEN      Coloring: auto, always, never [default: auto]
-  -h, --help        show this help message and exit
-";
+  /// print single column list
+  #[structopt(short = "1")]
+  single_column: bool,
 
-fn main() -> std::io::Result<()> {
-    let options: CommandsOptions = uvm_cli::get_options(USAGE)?;
-    uvm_commands::UvmCommand::new().exec(&options)
+  /// print only the path to the commands
+  #[structopt(short, long = "path")]
+  path_only: bool,
+
+  /// print debug output
+  #[structopt(short, long)]
+  debug: bool,
+
+  /// print more output
+  #[structopt(short, long, parse(from_occurrences))]
+  verbose: i32,
+
+  /// Color:.
+  #[structopt(short, long, possible_values = &ColorOption::variants(), case_insensitive = true, default_value)]
+  color: ColorOption,
+}
+
+fn main() -> Result<()> {
+  let opt = Opts::from_args();
+
+  set_colors_enabled(&opt.color);
+  set_loglevel(opt.debug.then(|| 2).unwrap_or(opt.verbose));
+
+  let commands = uvm_cli::find_sub_commands()?;
+  let out_style = Style::new().cyan();
+  let path_style = Style::new().italic().green();
+
+  let list = opt.list || opt.single_column || opt.verbose > 0;
+  let path_only = opt.path_only;
+  let single_column = opt.single_column;
+
+  let seperator = if list || !Term::stdout().is_term() {
+    "\n"
+  } else {
+    " "
+  };
+  let output = commands.fold(String::new(), |out, command| {
+    let mut new_line = out;
+
+    if !path_only || (list && !single_column) {
+      new_line += &format!("{}", out_style.apply_to(command.command_name().to_string()));
+    }
+
+    if list && !single_column {
+      new_line += " - ";
+    }
+
+    if path_only || (list && !single_column) {
+      new_line += &format!("{}", path_style.apply_to(command.path().display()));
+    }
+    new_line += seperator;
+    new_line
+  });
+  eprintln!("{}", &output);
+
+  Ok(())
 }
